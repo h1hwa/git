a8b6e72de364ec22aa4593ad99c2f797cc6e32f3
h1hwa <118655109+h1hwa@users.noreply.github.com>
2022-11-20 20:32:14 +0800

    pull

--- src/main/java/com/indi/GitTest.java
+++ src/main/java/com/indi/GitTest.java
@@ -7,7 +7,7 @@ public class GitTest {
         System.out.println("hello git2!");
         System.out.println("hello hot-fix!");
         System.out.println("hello master!");
-        System.out.println("hello hot-fixtest!");
         System.out.println("hello push!");
+        System.out.println("hello pull!");
     }
 }
